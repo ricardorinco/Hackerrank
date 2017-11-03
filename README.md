3fcc3ba1b0d23feb551b6e850be0aeb3cfebc853
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-11-03 13:55:13 -0200

    Challenge: A Very Big Sum

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
  2. [Simple Array Sum](https://www.hackerrank.com/challenges/simple-array-sum/)
  3. [Jim and the Orders](https://www.hackerrank.com/challenges/jim-and-the-orders/)
  4. [Order of Prime in Factorial](https://www.hackerrank.com/challenges/order-of-prime-in-factorial/)
+ 5. [A Very Big Sum](https://www.hackerrank.com/challenges/a-very-big-sum/)
 
---
+++ src/AVeryBigSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var elementsSize = Int32.Parse(Console.ReadLine());
+            var inputElements = Console.ReadLine().Split(' ');
+
+            var elements = Array.ConvertAll(inputElements, Int64.Parse);
+
+            Console.WriteLine(elements.Sum());
+
+            Console.ReadKey();
+        }
+    }
+}
