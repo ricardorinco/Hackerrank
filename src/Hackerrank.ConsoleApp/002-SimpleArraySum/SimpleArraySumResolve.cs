3e27b4407f1e6463848fdcad7f048b3674af884d
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-16 10:01:43 -0300

    Updated algorithm SimpleArraySum

---
+++ src/Hackerrank.ConsoleApp/002-SimpleArraySum/SimpleArraySumResolve.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Hackerrank.ConsoleApp._002_SimpleArraySum
+{
+    public class SimpleArraySumResolve
+    {
+        public static int SimpleArraySum(int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
---
+++ tests/Hackerrank.Tests/002-SimpleArraySum/SimpleArraySumResolveTests.cs
@@ -0,0 +1,18 @@
+﻿using Hackerrank.ConsoleApp._002_SimpleArraySum;
+using Xunit;
+
+namespace Hackerrank.Tests._002_SimpleArraySum
+{
+    public class SimpleArraySumResolveTests
+    {
+        [Theory]
+        [InlineData(6, new int[] { 1, 2, 3 })]
+        [InlineData(-55, new int[] { 21, 4, -80 })]
+        public void Should_be_sum_of_numbers_contains_in_array_expect_results_match(int expectedResult, int[] numbers)
+        {
+            int result = SimpleArraySumResolve.SimpleArraySum(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
