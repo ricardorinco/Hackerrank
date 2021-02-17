9fece7a6a91107f04666ed5b547f415e4cf54f77
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-17 06:05:56 -0300

    Updated algorithm MiniMaxSum

---
+++ src/Hackerrank.ConsoleApp/010-MiniMaxSum/MiniMaxSumResolve.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp._010_MiniMaxSum
+{
+    public class MiniMaxSumResolve
+    {
+        public static void MiniMaxSum(int[] numbers)
+        {
+            Array.Sort(numbers);
+            long min = numbers.Take(4).Sum(item => (long)item);
+            long max = numbers.Skip(1).Sum(item => (long)item);
+
+            Console.WriteLine($"{min} {max}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tests/Hackerrank.Tests/010-MiniMaxSumTests/MiniMaxSumResolveTests.cs
@@ -0,0 +1,9 @@
+﻿using Hackerrank.ConsoleApp._010_MiniMaxSum;
+using System.Text;
+using Xunit;
+
+namespace Hackerrank.Tests._010_MiniMaxSumTests
+{
+    public class MiniMaxSumResolveTests
+    { }
+}
