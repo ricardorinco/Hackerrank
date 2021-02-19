4ecd7c1a54277ff3af39dccfccbb2119f858e714
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-19 16:02:26 -0300

    Added algorithm Number Line Jumps

---
+++ src/Hackerrank.ConsoleApp/015-NumberLineJumps/NumberLineJumpsResolve.cs
@@ -0,0 +1,26 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public class NumberLineJumpsResolve
+    {
+        public static string Kangaroo(int start1, int jumpDistance1, int start2, int jumpDistance2)
+        {
+            if (start2 > start1 && jumpDistance2 > jumpDistance1)
+                return "NO";
+            
+            if (start1 < start2 && jumpDistance1 < jumpDistance2)
+                return "NO";
+            
+            if (jumpDistance1 == jumpDistance2)
+                return "NO";
+
+            if (
+                (start2 - start1) % (jumpDistance1 - jumpDistance2) == 0
+                || (start2 - start1) % (jumpDistance2 - jumpDistance1) == 0)
+            {
+                return "YES";
+            }
+
+            return "NO";
+        }
+    }
+}
