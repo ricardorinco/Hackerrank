64b25f70acdd4ba4d6e0a8af368b00e17191565d
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-19 16:03:26 -0300

    Added algorithm Number Line Jumps

---
+++ tests/Hackerrank.Tests/015-NumberLineJumpsTests/NumberLineJumpsResolveTests.cs
@@ -0,0 +1,24 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class NumberLineJumpsResolveTests
+    {
+        [Theory]
+        [InlineData("YES", 0, 3, 4, 2)]
+        [InlineData("NO", 0, 2, 5, 3)]
+        public void Should_be_obtains_the_same_land_location_after_same_number_of_jumps_expect_results_match(
+            string expectedResult,
+            int start1,
+            int jumpDistance1,
+            int start2,
+            int jumpDistance2
+        )
+        {
+            var canLandSameLocation = NumberLineJumpsResolve.Kangaroo(start1, jumpDistance1, start2, jumpDistance2);
+
+            Assert.Equal(expectedResult, canLandSameLocation);
+        }
+    }
+}
