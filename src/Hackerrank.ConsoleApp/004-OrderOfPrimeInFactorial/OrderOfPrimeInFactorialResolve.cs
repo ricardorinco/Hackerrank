763a854b534c9c2dc87a8a13bbdc7e6aad075bcf
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-21 14:49:42 -0300

    Update algorithm Order of Prime in Factorial

---
+++ src/Hackerrank.ConsoleApp/004-OrderOfPrimeInFactorial/OrderOfPrimeInFactorialResolve.cs
@@ -0,0 +1,36 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public static class OrderOfPrimeInFactorialResolve
+    {
+        public static long Solve(long p, long l)
+        {
+            long divisibleByP = 0;
+            long auxiliary = 0;
+
+            var resultA = l / p / p;
+            var resultB = l / p % p;
+            var resultC = l % p;
+
+            if (resultA > 0)
+                divisibleByP += resultA * p;
+
+            while (resultA > 0)
+            {
+                auxiliary += resultA;
+                resultA /= p;
+            }
+
+            auxiliary %= p;
+
+            if (auxiliary > 0)
+                auxiliary = p - auxiliary;
+
+            if (auxiliary < resultB)
+                divisibleByP += p;
+            else if (auxiliary == resultB)
+                divisibleByP += resultC + 1;
+
+            return divisibleByP - 1;
+        }
+    }
+}
---
+++ tests/Hackerrank.Tests/004-OrderOfPrimeInFactorialTests/OrderOfPrimeInFactorialResolveTest.cs
@@ -0,0 +1,18 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class OrderOfPrimeInFactorialResolveTest
+    {
+        [Theory]
+        [InlineData(2, 2, 6)]
+        [InlineData(2, 3, 6)]
+        public void Should_be_number_of_times_appears_in_the_prime_factorization_expect_results_match(int expectedResult, long p, long l)
+        {
+            long result = OrderOfPrimeInFactorialResolve.Solve(p, l);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
