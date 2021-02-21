1b7fd6be93779248c859ea6144db90ac3aa5ee3a
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-21 14:58:24 -0300

    Update algorithm A Very Big Sum

---
+++ src/Hackerrank.ConsoleApp/005-AVeryBigSum/AVeryBigSumResolve.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class AVeryBigSumResolve
+    {
+        public static long AVeryBigSum(long[] ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
---
+++ tests/Hackerrank.Tests/005-AVeryBigSumTests/AVeryBigSumResolveTests.cs
@@ -0,0 +1,17 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests._005_AVeryBigSumTests
+{
+    public class AVeryBigSumResolveTests
+    {
+        [Theory]
+        [InlineData(5000000015, new long[] { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005, })]
+        public void Should_be_calculate_of_elements_in_array_expect_results_match(long expectedResult, long[] numbers)
+        {
+            long result = AVeryBigSumResolve.AVeryBigSum(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
