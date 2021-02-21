174c26c1d077b77832511367c5b33b980bbf9d20
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-21 11:31:45 -0300

    Updated algorithm Jim and the Orders

---
+++ src/Hackerrank.ConsoleApp/003-JimAndTheOrders/Fan.cs
@@ -0,0 +1,9 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public class Fan
+    {
+        public int Id { get; set; }
+        public int Time { get; set; }
+        public int OrderTake { get; set; }
+    }
+}
