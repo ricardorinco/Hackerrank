27c19b60e66a8927f8d80ced32baf27a0b4580aa
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-21 11:31:51 -0300

    Updated algorithm Jim and the Orders

---
+++ src/Hackerrank.ConsoleApp/003-JimAndTheOrders/JimAndTheOrdersResolve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class JimAndTheOrdersResolve
+    {
+        public static int[] JimOrders(int[][] orders)
+        {
+            var fansList = new List<Fan>();
+            for (int i = 0; i < orders.Length; i++)
+            {
+                fansList.Add(new Fan {
+                    Id = i + 1,
+                    Time = orders[i][0],
+                    OrderTake = orders[i][1]
+                });
+            }
+
+            return fansList
+                .OrderBy(f => f.Time + f.OrderTake)
+                .Select(f => f.Id)
+                .ToArray();
+        }
+    }
+}
