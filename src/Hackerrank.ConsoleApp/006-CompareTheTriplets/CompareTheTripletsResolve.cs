1b964f7aa4cc64c72550590fb079de1d4c54dafb
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-23 06:45:31 -0300

    Update algorithm Compare the Triplets

---
+++ src/Hackerrank.ConsoleApp/006-CompareTheTriplets/CompareTheTripletsResolve.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class CompareTheTripletsResolve
+    {
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            var points = new List<int>();
+            var personOne = new Triplet() { One = a[0], Two = a[1], Three = a[2] };
+            var personTwo = new Triplet() { One = b[0], Two = b[1], Three = b[2] };
+
+            if (personOne.One > personTwo.One)
+                personOne.AddPoint();
+            else if (personOne.One != personTwo.One)
+                personTwo.AddPoint();
+
+            if (personOne.Two > personTwo.Two)
+                personOne.AddPoint();
+            else if (personOne.Two != personTwo.Two)
+                personTwo.AddPoint();
+
+            if (personOne.Three > personTwo.Three)
+                personOne.AddPoint();
+            else if (personOne.Three != personTwo.Three)
+                personTwo.AddPoint();
+
+            points.AddRange(new List<int>() { personOne.Points, personTwo.Points });
+
+            return points;
+        }
+    }
+}
---
+++ src/Hackerrank.ConsoleApp/006-CompareTheTriplets/Triplet.cs
@@ -0,0 +1,19 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public class Triplet
+    {
+        public int One { get; set; }
+        public int Two { get; set; }
+        public int Three { get; set; }
+
+        private int points;
+        public int Points
+        {
+            get { return points; }
+        }
+        public void AddPoint()
+        {
+            points += 1;
+        }
+    }
+}
