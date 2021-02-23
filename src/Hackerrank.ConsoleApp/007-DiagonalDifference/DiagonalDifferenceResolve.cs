fc40ab9aead9f2ea1cba217040ac28e91a5af339
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-23 06:58:48 -0300

    Update algorithm Diagonal Difference

---
+++ src/Hackerrank.ConsoleApp/007-DiagonalDifference/DiagonalDifferenceResolve.cs
@@ -0,0 +1,34 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public static class DiagonalDifferenceResolve
+    {
+        public static int DiagonalDifference(int[][] matrix)
+        {
+            var result = (SumPrimaryDiagonal(matrix) - SumSecondaryDiagonal(matrix)).ToString().Replace("-", "");
+
+            return int.Parse(result);
+        }
+
+        private static int SumPrimaryDiagonal(int[][] matrix)
+        {
+            var sum = 0;
+            for (int i = 0; i < matrix.Length; i++)
+                sum += matrix[i][i];
+
+            return sum;
+        }
+        private static int SumSecondaryDiagonal(int[][] matrix)
+        {
+            var sum = 0;
+            var count = matrix.Length - 1;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                sum += matrix[i][count];
+
+                count--;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ tests/Hackerrank.Tests/007-DiagonalDifference/DiagonalDifferenceResolveTests.cs
@@ -0,0 +1,23 @@
+﻿using Hackerrank.ConsoleApp;
+using System;
+using Xunit;
+
+
+namespace Hackerrank.Tests
+{
+    public class DiagonalDifferenceResolveTests
+    {
+        [Fact]
+        public void Should_be_calculate_the_absolute_difference_between_the_sums_of_diagonals()
+        {
+            var matrix = new int[3][];
+            matrix[0] = Array.ConvertAll("11 2 4".Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
+            matrix[1] = Array.ConvertAll("4 5 6".Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
+            matrix[2] = Array.ConvertAll("10 8 -12".Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
+
+            var result = DiagonalDifferenceResolve.DiagonalDifference(matrix);
+
+            Assert.Equal(15, result);
+        }
+    }
+}
