e360830255dc0b0d4d7a34ca2606f0993349fc91
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-23 07:08:50 -0300

    Update algorithm Plus Minus

---
+++ src/Hackerrank.ConsoleApp/008-PlusMinus/PlusMinusResolve.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class PlusMinusResolve
+    {
+        public static void PlusMinus(int[] arr)
+        {
+            Console.WriteLine(string.Format("{0:0.000000}", GetPositiveResult(arr.Length, arr)));
+            Console.WriteLine(string.Format("{0:0.000000}", GetNegativeResult(arr.Length, arr)));
+            Console.WriteLine(string.Format("{0:0.000000}", GetZeroResult(arr.Length, arr)));
+        }
+
+        private static decimal GetPositiveResult(int arraySize, int[] inputs)
+        {
+            var count = 0;
+            foreach (var input in inputs.Where(i => i > 0))
+                count++;
+
+            return Math.Round(decimal.Divide(count, arraySize), 6);
+        }
+        private static decimal GetNegativeResult(int arraySize, int[] inputs)
+        {
+            var count = 0;
+            foreach (var input in inputs.Where(i => i < 0))
+                count++;
+
+            return Math.Round(Decimal.Divide(count, arraySize), 6);
+        }
+        private static decimal GetZeroResult(int arraySize, int[] inputs)
+        {
+            var count = 0;
+            foreach (var input in inputs.Where(i => i == 0))
+                count++;
+
+            return Math.Round(Decimal.Divide(count, arraySize), 6);
+        }
+    }
+}
---
+++ tests/Hackerrank.Tests/008-PlusMinusTestes/PlusMinusResolveTests.cs
@@ -0,0 +1,5 @@
+﻿namespace Hackerrank.Tests
+{
+    public class PlusMinusResolveTests
+    { }
+}
