e05f721d21a2b8167858b84eefdd3e0b16393c1c
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-24 05:56:09 -0300

    Added algorithm Breaking the Records

---
+++ src/Hackerrank.ConsoleApp/017-BreakingTheRecords/BreakingTheRecordsResolve.cs
@@ -0,0 +1,30 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public class BreakingTheRecordsResolve
+    {
+        public static int[] BreakingRecords(int[] scores)
+        {
+            var mostRecord = scores[0];
+            var leastRecord = scores[0];
+
+            var mostRecordBreak = 0;
+            var leastRecordBreak = 0;
+
+            for (int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > mostRecord)
+                {
+                    mostRecord = scores[i];
+                    mostRecordBreak++;
+                }
+
+                if (scores[i] < leastRecord)
+                {
+                    leastRecord = scores[i];
+                    leastRecordBreak++;
+                }
+            }
+            return new int[] { mostRecordBreak, leastRecordBreak };
+        }
+    }
+}
