ddfb2cf89235eef6fd1f90264e0a20024f54a077
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-25 06:27:38 -0300

    Added algorithm Subarray Division

---
+++ src/Hackerrank.ConsoleApp/018-SubarrayDivision/SubarrayDivisionResolve.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class SubarrayDivisionResolve
+    {
+        public static int Birthday(List<int> squares, int d, int m)
+        {
+            var ways = 0;
+            var sum = 0;
+
+            if (m <= squares.Count)
+                for (var i = 0; i < m; i++) sum += squares[i];
+            
+            if (sum == d) ways++;
+            
+            for (var i = 0; i < squares.Count - m; i++)
+            {
+                sum = sum - squares[i] + squares[i + m];
+                if (sum == d) ways++;
+            }
+            
+            return ways;
+
+            // var ways = 0;
+            // var sum = 0;
+            //
+            // if (m <= squares.Count)
+            //     for (var i = 0; i < m; i++)
+            //         sum += squares[i];
+            // if (sum == d) ways++;
+            //
+            // for (var i = 0; i < squares.Count - m; i++)
+            // {
+            //     sum = sum - squares[i] + squares[i + m];
+            //     if (sum == d) ways++;
+            // }
+            //
+            // return ways;
+        }
+    }
+}
