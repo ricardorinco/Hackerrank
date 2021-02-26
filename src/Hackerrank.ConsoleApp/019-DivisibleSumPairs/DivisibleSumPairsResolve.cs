d79f111a1be2673965f69d8cbdbc58df3fd44a77
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-26 07:44:01 -0300

    Added algorithm Divisible Sum Pairs

---
+++ src/Hackerrank.ConsoleApp/019-DivisibleSumPairs/DivisibleSumPairsResolve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class DivisibleSumPairsResolve
+    {
+        public static int DivisibleSumPairs(int length, int divisor, int[] numbers)
+        {
+            var pairs = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (((numbers[i] + numbers[j]) % divisor) == 0)
+                        pairs++;
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
