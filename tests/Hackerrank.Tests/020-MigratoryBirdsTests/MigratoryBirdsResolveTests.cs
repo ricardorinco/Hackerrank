72ac4e37d35b7c55ceb51018d52508ec471d96f1
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-28 20:41:28 -0300

    Added algorithm Migratory Birds

---
+++ tests/Hackerrank.Tests/020-MigratoryBirdsTests/MigratoryBirdsResolveTests.cs
@@ -0,0 +1,30 @@
+﻿using Hackerrank.ConsoleApp;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class MigratoryBirdsResolveTests
+    {
+        [Theory, MemberData(nameof(DataBirds))]
+        public void Should_be_obtain_id_of_the_most_frequently_sighted_type_expect_results_match(
+            int expectedResult,
+            List<int> birds
+        )
+        {
+            var result = MigratoryBirdsResolve.MigratoryBirds(birds);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> DataBirds
+        {
+            get
+            {
+                yield return new object[] { 4, new List<int> { 1, 4, 4, 4, 5, 3 } };
+                yield return new object[] { 2, new List<int> { 2, 1, 1, 3, 2, 3, 4, 5, 2, 5 } };
+                yield return new object[] { 3, new List<int> { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 } };
+            }
+        }
+    }
+}
