3654bcb4d00d85fbfb1385912612187d34cfedf6
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-02-28 20:41:33 -0300

    Added algorithm Migratory Birds

---
+++ src/Hackerrank.ConsoleApp/020-MigratoryBirds/MigratoryBirdsResolve.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class MigratoryBirdsResolve
+    {
+        public static int MigratoryBirds(List<int> arr)
+        {
+            var result = arr.GroupBy(x => x).OrderByDescending(x => x.Count());
+
+            return result == null ? 0 : result.FirstOrDefault().Key;
+        }
+    }
+}
