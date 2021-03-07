fd7653d720c12a537b92debdee2f49db39b686e5
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-07 07:07:38 -0300

    Added MiniMaxSumResolveTests

--- tests/Hackerrank.Tests/010-MiniMaxSumTests/MiniMaxSumResolveTests.cs
+++ tests/Hackerrank.Tests/010-MiniMaxSumTests/MiniMaxSumResolveTests.cs
@@ -1,9 +1,33 @@
-﻿using Hackerrank.ConsoleApp;
-using System.Text;
+﻿using Hackerrank.Common;
+using Hackerrank.ConsoleApp;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Hackerrank.Tests
 {
     public class MiniMaxSumResolveTests
-    { }
+    {
+        [Theory, MemberData(nameof(DataNumber))]
+        public void Should_be_obtains_sum_min_and_max_numbers_expect_results_match(
+            string resultExpectedResult,
+            AbstractConsole console,
+            int[] numbers
+        )
+        {
+            MiniMaxSumResolve.MiniMaxSum(console, numbers);
+
+            var result = console.Contents[0];
+
+            Assert.Equal(resultExpectedResult.ToString(), result);
+        }
+
+        public static IEnumerable<object[]> DataNumber
+        {
+            get
+            {
+                yield return new object[] { "16 24", new TestConsole(), new int[] { 1, 3, 5, 7, 9 } };
+                yield return new object[] { "10 14", new AppConsole(), new int[] { 1, 2, 3, 4, 5 } };
+            }
+        }
+    }
 }
