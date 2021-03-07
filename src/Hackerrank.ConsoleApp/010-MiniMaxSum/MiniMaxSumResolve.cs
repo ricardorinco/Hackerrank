d1ba70dd1bde81b7ba74f4778a688b5e8ce54728
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-07 07:06:47 -0300

    Added test for class MiniMaxSumResolve

--- src/Hackerrank.ConsoleApp/010-MiniMaxSum/MiniMaxSumResolve.cs
+++ src/Hackerrank.ConsoleApp/010-MiniMaxSum/MiniMaxSumResolve.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using Hackerrank.Common;
+using System;
 using System.Linq;
 
 namespace Hackerrank.ConsoleApp
 {
     public static class MiniMaxSumResolve
     {
-        public static void MiniMaxSum(int[] numbers)
+        public static void MiniMaxSum(IConsole console, int[] numbers)
         {
             Array.Sort(numbers);
             long min = numbers.Take(4).Sum(item => (long)item);
             long max = numbers.Skip(1).Sum(item => (long)item);
 
-            Console.WriteLine($"{min} {max}");
-            Console.ReadKey();
+            console.WriteLine($"{min} {max}");
         }
     }
 }
