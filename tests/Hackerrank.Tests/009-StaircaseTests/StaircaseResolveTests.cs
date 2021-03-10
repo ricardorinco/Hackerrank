1edc0f250d52c785c696076deb55145152d15d80
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-10 20:06:00 -0300

    Added StaircaseResolveTests

--- tests/Hackerrank.Tests/009-StaircaseTests/StaircaseResolveTests.cs
+++ tests/Hackerrank.Tests/009-StaircaseTests/StaircaseResolveTests.cs
@@ -1,9 +1,32 @@
-﻿using Hackerrank.ConsoleApp;
-using System.Text;
+﻿using Hackerrank.Common;
+using Hackerrank.ConsoleApp;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Hackerrank.Tests
 {
     public class StaircaseResolveTests
-    { }
+    {
+        [Theory, MemberData(nameof(DataStar))]
+        public void Should_be_obtains_the_draw_of_star_expect_results_match(
+            List<string> expectedResults,
+            AbstractConsole console,
+            int staircaseSize
+        )
+        {
+            StaircaseResolve.Resolve(console, staircaseSize);
+
+            for (int i = 0; i < console.Contents.Count; i++)
+                Assert.Equal(expectedResults[i], console.Contents[i]);
+        }
+
+        public static IEnumerable<object[]> DataStar
+        {
+            get
+            {
+                yield return new object[] { new List<string> { "   #", "  ##", " ###", "####" }, new TestConsole(), 4 };
+                yield return new object[] { new List<string> { "     #", "    ##", "   ###", "  ####", " #####", "######" }, new TestConsole(), 6 };
+            }
+        }
+    }
 }
