fc4507f854e6b5ab47d58845e20544e40f431515
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-16 05:50:25 -0300

    Update DayOfTheProgrammerResolveTests

--- tests/Hackerrank.Tests/021-DayOfTheProgrammerTests/DayOfTheProgrammerResolveTests.cs
+++ tests/Hackerrank.Tests/021-DayOfTheProgrammerTests/DayOfTheProgrammerResolveTests.cs
@@ -7,8 +7,16 @@ namespace Hackerrank.Tests
     {
         [Theory]
         [InlineData("12.09.1800", 1800)]
+        [InlineData("12.09.1900", 1900)]
+        [InlineData("13.09.1915", 1915)]
+        [InlineData("12.09.1916", 1916)]
+        [InlineData("26.09.1918", 1918)]
+        [InlineData("13.09.1986", 1986)]
+        [InlineData("12.09.1996", 1996)]
+        [InlineData("12.09.2000", 2000)]
         [InlineData("12.09.2016", 2016)]
         [InlineData("13.09.2017", 2017)]
+        [InlineData("13.09.2021", 2021)]
         public void Should_be_obtain_the_data_of_256_day_of_the_year_expect_results_match(string expectedResult, int year)
         {
             var result = DayOfTheProgrammerResolve.DayOfProgrammer(year);
