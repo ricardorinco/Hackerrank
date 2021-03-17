704421c711cbaaf95e945e310e168e6d213a24e6
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-17 19:22:53 -0300

    Added algorithm Bill Division

---
+++ src/Hackerrank.ConsoleApp/022-BillDivision/BillDivisionResolve.cs
@@ -0,0 +1,27 @@
+﻿using Hackerrank.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class BillDivisionResolve
+    {
+        public static void BonAppetit(
+            IConsole console,
+            List<int> bill,
+            int k,
+            int b
+        )
+        {
+            var valueOfSharedItems = bill
+                .Select((value, index) => new { value, index })
+                .Where(x => x.index != k)
+                .Sum(x => x.value) / 2;
+
+            if (b == valueOfSharedItems)
+                console.WriteLine("Bon Appetit");
+            else
+                console.WriteLine($"{b - valueOfSharedItems}");
+        }
+    }
+}
