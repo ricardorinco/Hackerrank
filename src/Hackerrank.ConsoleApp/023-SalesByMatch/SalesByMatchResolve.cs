8235ebebb3b3a9092211836bee961bb86b8ef4b3
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-18 20:03:05 -0300

    Added algorithm Sales by Match

---
+++ src/Hackerrank.ConsoleApp/023-SalesByMatch/SalesByMatchResolve.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class SalesByMatchResolve
+    {
+        public static int SockMerchant(int n, int[] ar)
+        {
+            return ar
+                .GroupBy(x => x)
+                .Select(x => new { pairs = x.Count() / 2 })
+                .Sum(x => x.pairs);
+        }
+    }
+}
