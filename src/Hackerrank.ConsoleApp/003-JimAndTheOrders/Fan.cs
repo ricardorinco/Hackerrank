ef14a7b7cad5a1db0d9514c57a9892c578b1f184
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-19 07:43:16 -0300

    Refactoring algorithm JimAndTheOrders

--- src/Hackerrank.ConsoleApp/003-JimAndTheOrders/Fan.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hackerrank.ConsoleApp
-{
-    public class Fan
-    {
-        public int Id { get; set; }
-        public int Time { get; set; }
-        public int OrderTake { get; set; }
-    }
-}
