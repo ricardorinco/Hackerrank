95d21b023fb8509de0dffb90cbf3296c4a40b57b
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-19 07:44:36 -0300

    Refactoring algorithm SimpleArraySum

--- tests/Hackerrank.Tests/002-SimpleArraySumTests/SimpleArraySumResolveTests.cs
+++ tests/Hackerrank.Tests/002-SimpleArraySumTests/SimpleArraySumResolveTests.cs
@@ -1,4 +1,5 @@
-﻿using Hackerrank.ConsoleApp;
+﻿using System.Collections.Generic;
+using Hackerrank.ConsoleApp;
 using Xunit;
 
 namespace Hackerrank.Tests
@@ -6,11 +7,14 @@ namespace Hackerrank.Tests
     public class SimpleArraySumResolveTests
     {
         [Theory]
-        [InlineData(6, new int[] { 1, 2, 3 })]
-        [InlineData(-55, new int[] { 21, 4, -80 })]
-        public void Should_be_sum_of_numbers_contains_in_array_expect_results_match(int expectedResult, int[] numbers)
+        [InlineData(6, new[] { 1, 2, 3 })]
+        [InlineData(-55, new[] { 21, 4, -80 })]
+        public void Should_be_sum_of_numbers_contains_in_array_expect_results_match(
+            int expectedResult,
+            IEnumerable<int> numbers
+        )
         {
-            int result = SimpleArraySumResolve.SimpleArraySum(numbers);
+            var result = SimpleArraySumResolve.SimpleArraySum(numbers);
 
             Assert.Equal(expectedResult, result);
         }
