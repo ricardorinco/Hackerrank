e877d02c7387f66424de6777134af9a1b9712c6b
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-19 07:44:19 -0300

    Refactoring algorithm SimpleArraySum

--- src/Hackerrank.ConsoleApp/002-SimpleArraySum/SimpleArraySumResolve.cs
+++ src/Hackerrank.ConsoleApp/002-SimpleArraySum/SimpleArraySumResolve.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Hackerrank.ConsoleApp
 {
     public static class SimpleArraySumResolve
     {
-        public static int SimpleArraySum(int[] numbers)
+        public static int SimpleArraySum(IEnumerable<int> numbers)
         {
             return numbers.Sum();
         }
