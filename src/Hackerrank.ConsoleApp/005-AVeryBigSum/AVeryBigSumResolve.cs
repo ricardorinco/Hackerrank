10beeb9bd0d6cffe298b53c52e3ced9b701934da
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-20 21:33:21 -0300

    Refactoring algorithm AVeryBigSum

--- src/Hackerrank.ConsoleApp/005-AVeryBigSum/AVeryBigSumResolve.cs
+++ src/Hackerrank.ConsoleApp/005-AVeryBigSum/AVeryBigSumResolve.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Hackerrank.ConsoleApp
 {
     public static class AVeryBigSumResolve
     {
-        public static long AVeryBigSum(long[] ar)
+        public static long AVeryBigSum(IEnumerable<long> ar)
         {
             return ar.Sum();
         }
