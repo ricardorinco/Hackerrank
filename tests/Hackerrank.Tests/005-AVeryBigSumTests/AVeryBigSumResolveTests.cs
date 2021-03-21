b5c16eaea5be7f647bd7e10ccb2dc8909eb99642
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-20 21:33:28 -0300

    Refactoring algorithm AVeryBigSum

--- tests/Hackerrank.Tests/005-AVeryBigSumTests/AVeryBigSumResolveTests.cs
+++ tests/Hackerrank.Tests/005-AVeryBigSumTests/AVeryBigSumResolveTests.cs
@@ -9,7 +9,7 @@ namespace Hackerrank.Tests
         [InlineData(5000000015, new long[] { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005, })]
         public void Should_be_calculate_of_elements_in_array_expect_results_match(long expectedResult, long[] numbers)
         {
-            long result = AVeryBigSumResolve.AVeryBigSum(numbers);
+            var result = AVeryBigSumResolve.AVeryBigSum(numbers);
 
             Assert.Equal(expectedResult, result);
         }
