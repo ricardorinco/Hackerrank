310e8a7184bba84f0f99838d020fc4c563ae18c2
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-21 07:05:51 -0300

    Refactoring project Hackerrank.Tests

---
+++ tests/Hackerrank.Tests/018-SubarrayDivision/SubarrayDivisionResolveTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class SubarrayDivisionResolveTests
+    {
+        [Theory, MemberData(nameof(DataChocolateBar))]
+        public void Should_be_the_bars_number_of_division_expect_results_match(
+            int expectedResult,
+            List<int> squares,
+            int d,
+            int m
+        )
+        {
+            var result = SubarrayDivisionResolve.Birthday(squares, d, m);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> DataChocolateBar
+        {
+            get
+            {
+                yield return new object[] { 2, new List<int> { 1, 2, 1, 3, 2 }, 3, 2 };
+                yield return new object[] { 0, new List<int> { 1, 1, 1, 1, 1, 1 }, 3, 2 };
+                yield return new object[] { 1, new List<int> { 4 }, 4, 1 };
+            }
+        }
+    }
+}
