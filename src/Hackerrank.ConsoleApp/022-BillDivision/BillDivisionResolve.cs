5340e3638ce910fe314e655deb67c57a64108f18
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-20 21:35:06 -0300

    Refactoring algorithm BillDivision

--- src/Hackerrank.ConsoleApp/022-BillDivision/BillDivisionResolve.cs
+++ src/Hackerrank.ConsoleApp/022-BillDivision/BillDivisionResolve.cs
@@ -1,17 +1,12 @@
-﻿using Hackerrank.Common.Console.Interfaces;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using Hackerrank.Common.Console.Interfaces;
 
 namespace Hackerrank.ConsoleApp
 {
     public static class BillDivisionResolve
     {
-        public static void BonAppetit(
-            IConsole console,
-            List<int> bill,
-            int k,
-            int b
-        )
+        public static void BonAppetit(IConsole console, List<int> bill, int k, int b)
         {
             var valueOfSharedItems = bill
                 .Select((value, index) => new { value, index })
