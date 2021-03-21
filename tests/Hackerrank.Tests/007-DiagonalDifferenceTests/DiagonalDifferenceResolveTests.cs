9da7bcbb6761eac15fc2114069404ca9ab69c76a
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-21 07:00:17 -0300

    Refactoring algorithm DiagonalDifference

--- tests/Hackerrank.Tests/007-DiagonalDifferenceTests/DiagonalDifferenceResolveTests.cs
+++ tests/Hackerrank.Tests/007-DiagonalDifferenceTests/DiagonalDifferenceResolveTests.cs
@@ -11,9 +11,9 @@ namespace Hackerrank.Tests
         public void Should_be_calculate_the_absolute_difference_between_the_sums_of_diagonals()
         {
             var matrix = new int[3][];
-            matrix[0] = Array.ConvertAll("11 2 4".Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
-            matrix[1] = Array.ConvertAll("4 5 6".Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
-            matrix[2] = Array.ConvertAll("10 8 -12".Split(' '), ordersTemp => Convert.ToInt32(ordersTemp));
+            matrix[0] = Array.ConvertAll("11 2 4".Split(' '), Convert.ToInt32);
+            matrix[1] = Array.ConvertAll("4 5 6".Split(' '), Convert.ToInt32);
+            matrix[2] = Array.ConvertAll("10 8 -12".Split(' '), Convert.ToInt32);
 
             var result = DiagonalDifferenceResolve.DiagonalDifference(matrix);
 
