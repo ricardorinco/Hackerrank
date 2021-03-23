c662e13bff9925c003f3f82637e1d5a0e5b6a7a4
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-23 05:54:35 -0300

    Added algorithm Find the Point

---
+++ src/Hackerrank.ConsoleApp/025-FindThePoint/FindThePointResolve.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class FindThePointResolve
+    {
+        public static int[] FindPoint(int px, int py, int qx, int qy)
+        {
+            return new List<int> { 2 * qx - px, 2 * qy - py }.ToArray();
+        }
+    }
+}
