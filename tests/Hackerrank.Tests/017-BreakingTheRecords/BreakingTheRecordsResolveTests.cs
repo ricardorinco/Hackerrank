35e8589a210985b13a5a2b4e43518a418014f3b7
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-24 11:26:37 -0300

    Refactoring algorithm BreakingTheRecords

--- tests/Hackerrank.Tests/017-BreakingTheRecords/BreakingTheRecordsResolveTests.cs
+++ tests/Hackerrank.Tests/017-BreakingTheRecords/BreakingTheRecordsResolveTests.cs
@@ -6,9 +6,9 @@ namespace Hackerrank.Tests
     public class BreakingTheRecordsResolveTests
     {
         [Theory]
-        [InlineData(new int[] { 1, 1 }, new int[] { 12, 24, 10, 24 })]
-        [InlineData(new int[] { 2, 4 }, new int[] { 10, 5, 20, 20, 4, 5, 2, 25, 1 })]
-        [InlineData(new int[] { 4, 0 }, new int[] { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42, })]
+        [InlineData(new[] { 1, 1 }, new[] { 12, 24, 10, 24 })]
+        [InlineData(new[] { 2, 4 }, new[] { 10, 5, 20, 20, 4, 5, 2, 25, 1 })]
+        [InlineData(new[] { 4, 0 }, new[] { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42, })]
         public void Should_be_find_the_number_of_times_breaks_records_for_most_and_least_points(
             int[] expectedResult,
             int[] scores
