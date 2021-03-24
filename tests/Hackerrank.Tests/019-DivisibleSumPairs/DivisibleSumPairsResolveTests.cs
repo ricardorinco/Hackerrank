80d4a1dc94a78a845f98400fae275cc1d45345d8
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-24 11:26:50 -0300

    Refactoring algorithm DivisibleSumPairs

--- tests/Hackerrank.Tests/019-DivisibleSumPairs/DivisibleSumPairsResolveTests.cs
+++ tests/Hackerrank.Tests/019-DivisibleSumPairs/DivisibleSumPairsResolveTests.cs
@@ -6,8 +6,8 @@ namespace Hackerrank.Tests
     public class DivisibleSumPairsResolveTests
     {
         [Theory]
-        [InlineData(3, 6, 5, new int[] { 1, 2, 3, 4, 5, 6 })]
-        [InlineData(5, 6, 3, new int[] { 1, 3, 2, 6, 1, 2 })]
+        [InlineData(3, 6, 5, new[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(5, 6, 3, new[] { 1, 3, 2, 6, 1, 2 })]
         public void Should_be_obtains_the_number_of_possible_divisions_expect_results_match(
             int expectedResult,
             int length,
