ef476fac10a492e5dfd95c108b37dd8635d28825
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-24 11:27:10 -0300

    Refactoring algorithm SalesByMatch

--- tests/Hackerrank.Tests/023-SalesByMatch/SalesByMatchResolveTests.cs
+++ tests/Hackerrank.Tests/023-SalesByMatch/SalesByMatchResolveTests.cs
@@ -6,10 +6,10 @@ namespace Hackerrank.Tests
     public class SalesByMatchResolveTests
     {
         [Theory]
-        [InlineData(2, 7, new int[] { 1, 2, 1, 2, 1, 3, 2 })]
-        [InlineData(3, 9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 28 })]
-        [InlineData(4, 10, new int[] { 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 })]
-        [InlineData(4, 14, new int[] { 4, 8, 4, 8, 4, 8, 4, 4, 8, 7, 8, 9, 10, 22 })]
+        [InlineData(2, 7, new[] { 1, 2, 1, 2, 1, 3, 2 })]
+        [InlineData(3, 9, new[] { 10, 20, 20, 10, 10, 30, 50, 10, 28 })]
+        [InlineData(4, 10, new[] { 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 })]
+        [InlineData(4, 14, new[] { 4, 8, 4, 8, 4, 8, 4, 4, 8, 7, 8, 9, 10, 22 })]
         public void Should_be_find_the_number_of_pairs_expect_results_match(
             int expectedResult,
             int n,
