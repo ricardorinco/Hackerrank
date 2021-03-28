d9826e0ad9f03ee69716ae5ff50be0c7654d95ae
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-28 15:21:35 -0300

    Refactoring algorithm CountingValleys

---
+++ src/Hackerrank.ConsoleApp/026-CountingValleys/CountingValleysResolve.cs
@@ -0,0 +1,30 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public static class CountingValleysResolve
+    {
+        public static int CountingValleys(int steps, string path)
+        {
+			var altitude = 0;
+			var valleyCounter = 0;
+
+			for (int i = 0; i < steps; i++)
+			{
+				var letter = path[i];
+				if (letter == 'U')
+				{
+					altitude++;
+					if (altitude == 0)
+					{
+						valleyCounter++;
+					}
+				}
+				else
+				{
+					altitude--;
+				}
+			}
+
+			return valleyCounter;
+		}
+    }
+}
