26ad046ee662504a86cb377d3c37a243541e1236
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-29 06:17:10 -0300

    Added algorithm Electronics Shop

---
+++ src/Hackerrank.ConsoleApp/027-ElectronicsShop/ElectronicsShopResolve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class ElectronicsShopResolve
+    {
+        public static int GetMoneySpent(IEnumerable<int> keyboards, int[] drives, int budget)
+        {
+            var kitValue = 0;
+
+            foreach (var keyboard in keyboards)
+            {
+                foreach (var drive in drives)
+                {
+                    var kit = keyboard + drive;
+                    if (keyboard + drive <= budget && kitValue < kit)
+                    {
+                        kitValue = kit;
+                    }
+                }
+            }
+            
+            return kitValue == 0 ? -1 : kitValue;
+        }
+    }
+}
