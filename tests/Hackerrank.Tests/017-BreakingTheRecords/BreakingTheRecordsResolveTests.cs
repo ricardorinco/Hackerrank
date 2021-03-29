7218e7823f3eda0dd7eb0b7a96eb923345e23ec6
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-29 06:17:17 -0300

    Added algorithm Electronics Shop

--- tests/Hackerrank.Tests/017-BreakingTheRecords/BreakingTheRecordsResolveTests.cs
+++ tests/Hackerrank.Tests/017-BreakingTheRecords/BreakingTheRecordsResolveTests.cs
@@ -9,7 +9,7 @@ namespace Hackerrank.Tests
         [InlineData(new[] { 1, 1 }, new[] { 12, 24, 10, 24 })]
         [InlineData(new[] { 2, 4 }, new[] { 10, 5, 20, 20, 4, 5, 2, 25, 1 })]
         [InlineData(new[] { 4, 0 }, new[] { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42, })]
-        public void Should_be_find_the_number_of_times_breaks_records_for_most_and_least_points(
+        public void Should_be_find_the_number_of_times_breaks_records_for_most_and_least_points_expect_results_match(
             int[] expectedResult,
             int[] scores
         )
