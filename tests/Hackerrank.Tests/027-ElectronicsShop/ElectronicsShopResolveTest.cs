637a53778d8909e5a760d5f7d8ab21cfac7355aa
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-29 06:17:04 -0300

    Added algorithm Electronics Shop

---
+++ tests/Hackerrank.Tests/027-ElectronicsShop/ElectronicsShopResolveTest.cs
@@ -0,0 +1,24 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class ElectronicsShopResolveTest
+    {
+        [Theory]
+        [InlineData(58, new[] { 40, 50, 60 }, new[] { 5, 8, 12 }, 60)]
+        [InlineData(9, new[] { 3, 1 }, new[] { 5, 2, 8 }, 10)]
+        [InlineData(-1, new[] { 4 }, new[] { 5 }, 5)]
+        public void Should_be_find_the_most_expensive_keyboard_usb_driver_purchased_expect_results_match(
+            int expectedResult,
+            int[] keyboards,
+            int[] drives,
+            int budget
+        )
+        {
+            var kit = ElectronicsShopResolve.GetMoneySpent(keyboards, drives, budget);
+
+            Assert.Equal(expectedResult, kit);
+        }
+    }
+}
