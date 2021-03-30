7a8fbff385a4fe539c813d989291d09e8ac5cb9b
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-30 06:20:28 -0300

    Added algorithm Cats and a Mouse

---
+++ src/Hackerrank.ConsoleApp/028-CatsAndAMouse/CatsAndAMouseResolve.cs
@@ -0,0 +1,21 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public static class CatsAndAMouseResolve
+    {
+        public static string CatAndMouse(int catA, int catB, int mouse)
+        {
+            var distanceCatA = (catA - mouse).NonNegative();
+            var distanceCatB = (catB - mouse).NonNegative();
+
+            if (distanceCatA == distanceCatB)
+                return "Mouse C";
+            
+            return distanceCatA > distanceCatB ? "Cat B" : "Cat A";
+        }
+
+        private static int NonNegative(this int number)
+        {
+            return number > 0 ? number : number * -1;
+        }
+    }
+}
