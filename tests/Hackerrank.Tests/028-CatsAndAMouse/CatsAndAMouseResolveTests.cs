ff0179bcfc8036f5ed73e19a4e35c27ea617e694
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-03-30 06:20:34 -0300

    Added algorithm Cats and a Mouse

---
+++ tests/Hackerrank.Tests/028-CatsAndAMouse/CatsAndAMouseResolveTests.cs
@@ -0,0 +1,24 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class CatsAndAMouseResolveTests
+    {
+        [Theory]
+        [InlineData("Cat B", 2, 5, 4)]
+        [InlineData("Cat B", 1, 2, 3)]
+        [InlineData("Mouse C", 1, 3, 2)]
+        public void Should_be_the_position_of_cats_and_a_mouse_expect_results_match(
+            string expectedResult,
+            int catA,
+            int catB,
+            int mouse
+        )
+        {
+            var result = CatsAndAMouseResolve.CatAndMouse(catA, catB, mouse);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
