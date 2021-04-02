ba1dd7a2caf8262a77a167951d90b0e1af7292e9
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-02 16:10:07 -0300

    Added algorithm Forming a Magic Square

---
+++ src/Hackerrank.ConsoleApp/029-FormingMagicSquare/FormingMagicSquareResolve.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class FormingMagicSquareResolve
+    {
+        private static readonly int[,,] matrixCombination = new [,,] {
+            { {8, 1, 6 }, { 3, 5, 7 }, { 4, 9, 2 } },
+            { {6, 1, 8 }, { 7, 5, 3 }, { 2, 9, 4 } },
+            { {4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } },
+            { {2, 9, 4 }, { 7, 5, 3 }, { 6, 1, 8 } },
+            { {8, 3, 4 }, { 1, 5, 9 }, { 6, 7, 2 } },
+            { {4, 3, 8 }, { 9, 5, 1 }, { 2, 7, 6 } },
+            { {6, 7, 2 }, { 1, 5, 9 }, { 8, 3, 4 } },
+            { {2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } }
+        };
+
+        public static int FormingMagicSquare(int[][] matrix)
+        {
+            var minChanges = int.MaxValue;
+
+            for (var i = 0; i < matrix.Length; ++i) //o
+            {
+                int changeCount = 0;
+                for (var x = 0; x < matrix.Length; ++x)
+                {
+                    for (var y = 0; y < matrix.Length; ++y)
+                    {
+                        changeCount += Math.Abs(matrixCombination[i, y, x] - matrix[y][x]);
+                    }
+                }
+
+                minChanges = Math.Min(minChanges, changeCount);
+            }
+
+            return minChanges;
+        }
+    }
+}
