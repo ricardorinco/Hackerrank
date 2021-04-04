ba25eaea7239d6aebba8518d05323c970a682f3b
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-04 11:04:40 -0300

    Added test algorithm The Hurdle Race

---
+++ src/Hackerrank.ConsoleApp/030-TheHurdleRace/TheHurdleRaceResolve.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class TheHurdleRaceResolve
+    {
+        public static int HurdleRace(int k, int[] heights)
+        {
+            int max = heights.Max();
+            return Math.Max(max - k, 0);
+        }
+    }
+}
