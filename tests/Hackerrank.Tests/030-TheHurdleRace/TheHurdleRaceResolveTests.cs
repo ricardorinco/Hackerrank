d6e2c5651909e6a5cba4dbdc1d36b0b69adac057
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-04 11:04:34 -0300

    Added test algorithm The Hurdle Race

---
+++ tests/Hackerrank.Tests/030-TheHurdleRace/TheHurdleRaceResolveTests.cs
@@ -0,0 +1,23 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class TheHurdleRaceResolveTests
+    {
+        [Theory]
+        [InlineData(2, 1, new[] { 1, 2, 3, 3, 2 })]
+        [InlineData(2, 4, new[] { 1, 6, 3, 5, 2 })]
+        [InlineData(0, 7, new[] { 2, 5, 4, 5, 2 })]
+        public void Should_be_many_doses_potion_must_character_take_to_able_jump_all_hurdles_expect_results_match(
+            int expectedResult,
+            int k,
+            int[] heights
+        )
+        {
+            var valleys = TheHurdleRaceResolve.HurdleRace(k, heights);
+
+            Assert.Equal(expectedResult, valleys);
+        }
+    }
+}
