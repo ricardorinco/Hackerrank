19240e2556612f67365d1b9de7ebc7c8b9594800
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-05 06:48:07 -0300

    Added test algorithm Designer PDF Viewer

---
+++ src/Hackerrank.ConsoleApp/031-DesignerPdfViewer/DesignerPdfViewerResolve.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class DesignerPdfViewerResolve
+    {
+        private static readonly string[] Alphabet =
+        {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"
+        };
+
+        public static int DesignerPdfViewer(int[] heights, string word)
+        {
+            var tallestLetter = 0;
+            
+            foreach (var c in word.ToCharArray())
+            {
+                var wordPosition = Alphabet
+                    .Select((w, i) => new {i, w})
+                    .Where(x => x.w == c.ToString().ToLower())
+                    .Select(x => x.i)
+                    .FirstOrDefault();
+
+                var letterHeight = heights[wordPosition];
+
+                if (letterHeight > tallestLetter)
+                {
+                    tallestLetter = heights[wordPosition];    
+                }
+            }
+
+            return word.Length * 1 * tallestLetter;
+        }
+    }
+}
