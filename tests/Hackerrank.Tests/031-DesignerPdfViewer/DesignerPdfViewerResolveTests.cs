94dac146d04e8321609a4a75d29bd0bc152645a5
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-05 06:48:04 -0300

    Added test algorithm Designer PDF Viewer

---
+++ tests/Hackerrank.Tests/031-DesignerPdfViewer/DesignerPdfViewerResolveTests.cs
@@ -0,0 +1,23 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class DesignerPdfViewerResolveTests
+    {
+        [Theory]
+        [InlineData(8, new[] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 1, 1, 5, 5, 1, 5, 2, 5, 5, 5, 5, 5, 5 }, "torn")]
+        [InlineData(9, new[] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }, "abc")]
+        [InlineData(28, new[] { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 }, "zaba")]
+        public void Should_be_find_the_selection_area_for_word_expect_results_match(
+            int expectedResult,
+            int[] heights,
+            string word
+        )
+        {
+            var result = DesignerPdfViewerResolve.DesignerPdfViewer(heights, word);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
