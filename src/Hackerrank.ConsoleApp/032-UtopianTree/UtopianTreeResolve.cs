0503e67956953d4f95a063dc148c815eefbddd8b
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-11 15:41:47 -0300

    Added algorithm Utopian Tree

---
+++ src/Hackerrank.ConsoleApp/032-UtopianTree/UtopianTreeResolve.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Hackerrank.ConsoleApp
+{
+    public static class UtopianTreeResolve
+    {
+        public static int UtopianTree(int cycles)
+        {
+            var currentHeight = 1;
+            for (var i = 1; i <= cycles; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    currentHeight += 1;
+                }
+                else
+                {
+                    currentHeight *= 2;
+                }
+            }
+
+            return currentHeight;
+        }
+    }
+}
