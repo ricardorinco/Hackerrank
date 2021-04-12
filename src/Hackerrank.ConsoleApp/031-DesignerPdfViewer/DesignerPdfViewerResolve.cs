adeb779d944392d83cdb51af0bd8a02e30bd46e9
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-12 07:07:36 -0300

    Update DesignerPdfViewerResolve

--- src/Hackerrank.ConsoleApp/031-DesignerPdfViewer/DesignerPdfViewerResolve.cs
+++ src/Hackerrank.ConsoleApp/031-DesignerPdfViewer/DesignerPdfViewerResolve.cs
@@ -6,8 +6,8 @@ namespace Hackerrank.ConsoleApp
     {
         private static readonly string[] Alphabet =
         {
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-            "v", "w", "x", "y", "z"
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
         };
 
         public static int DesignerPdfViewer(int[] heights, string word)
