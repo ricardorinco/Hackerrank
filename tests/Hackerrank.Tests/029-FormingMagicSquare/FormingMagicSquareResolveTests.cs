369cf552722252ac9ceaa9694309a2a6df210074
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-12 08:01:59 -0300

    Refactoring algorithm FormingMagicSquare

--- tests/Hackerrank.Tests/029-FormingMagicSquare/FormingMagicSquareResolveTests.cs
+++ tests/Hackerrank.Tests/029-FormingMagicSquare/FormingMagicSquareResolveTests.cs
@@ -1,5 +1,6 @@
 ﻿using Hackerrank.ConsoleApp;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Hackerrank.Tests
@@ -9,10 +10,10 @@ namespace Hackerrank.Tests
         [Fact]
         public void Should_be_calculate_convert_minimal_cost_matrix_4_9_2__3_5_7__8_1_5()
         {
-            var matrix = new int[3][];
-            matrix[0] = Array.ConvertAll("4 9 2".Split(' '), Convert.ToInt32);
-            matrix[1] = Array.ConvertAll("3 5 7".Split(' '), Convert.ToInt32);
-            matrix[2] = Array.ConvertAll("8 1 5".Split(' '), Convert.ToInt32);
+            var matrix = new List<int>();
+            matrix.AddRange(Array.ConvertAll("4 9 2".Split(' '), Convert.ToInt32));
+            matrix.AddRange(Array.ConvertAll("3 5 7".Split(' '), Convert.ToInt32));
+            matrix.AddRange(Array.ConvertAll("8 1 5".Split(' '), Convert.ToInt32));
 
             var result = FormingMagicSquareResolve.FormingMagicSquare(matrix);
 
@@ -22,10 +23,10 @@ namespace Hackerrank.Tests
         [Fact]
         public void Should_be_calculate_convert_minimal_cost_matrix_4_8_2__4_5_7__6_1_6()
         {
-            var matrix = new int[3][];
-            matrix[0] = Array.ConvertAll("4 8 2".Split(' '), Convert.ToInt32);
-            matrix[1] = Array.ConvertAll("4 5 7".Split(' '), Convert.ToInt32);
-            matrix[2] = Array.ConvertAll("6 1 6".Split(' '), Convert.ToInt32);
+            var matrix = new List<int>();
+            matrix.AddRange(Array.ConvertAll("4 8 2".Split(' '), Convert.ToInt32));
+            matrix.AddRange(Array.ConvertAll("4 5 7".Split(' '), Convert.ToInt32));
+            matrix.AddRange(Array.ConvertAll("6 1 6".Split(' '), Convert.ToInt32));
 
             var result = FormingMagicSquareResolve.FormingMagicSquare(matrix);
 
