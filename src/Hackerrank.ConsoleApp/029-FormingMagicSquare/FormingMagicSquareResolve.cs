052cf371c854f5f013ecd26e0613a4f4ddfdd049
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-12 08:02:19 -0300

    Refactoring algorithm FormingMagicSquare

--- src/Hackerrank.ConsoleApp/029-FormingMagicSquare/FormingMagicSquareResolve.cs
+++ src/Hackerrank.ConsoleApp/029-FormingMagicSquare/FormingMagicSquareResolve.cs
@@ -1,39 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hackerrank.ConsoleApp
 {
     public static class FormingMagicSquareResolve
     {
-        private static readonly int[,,] matrixCombination = new [,,] {
-            { {8, 1, 6 }, { 3, 5, 7 }, { 4, 9, 2 } },
-            { {6, 1, 8 }, { 7, 5, 3 }, { 2, 9, 4 } },
-            { {4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } },
-            { {2, 9, 4 }, { 7, 5, 3 }, { 6, 1, 8 } },
-            { {8, 3, 4 }, { 1, 5, 9 }, { 6, 7, 2 } },
-            { {4, 3, 8 }, { 9, 5, 1 }, { 2, 7, 6 } },
-            { {6, 7, 2 }, { 1, 5, 9 }, { 8, 3, 4 } },
-            { {2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } }
+        private static readonly List<List<int>> Squares = new List<List<int>>
+        {
+            new List<int> {8, 1, 6, 3, 5, 7, 4, 9, 2},
+            new List<int> {6, 1, 8, 7, 5, 3, 2, 9, 4},
+            new List<int> {4, 9, 2, 3, 5, 7, 8, 1, 6},
+            new List<int> {2, 9, 4, 7, 5, 3, 6, 1, 8},
+            new List<int> {8, 3, 4, 1, 5, 9, 6, 7, 2},
+            new List<int> {4, 3, 8, 9, 5, 1, 2, 7, 6},
+            new List<int> {6, 7, 2, 1, 5, 9, 8, 3, 4},
+            new List<int> {2, 7, 6, 9, 5, 1, 4, 3, 8}
         };
 
-        public static int FormingMagicSquare(int[][] matrix)
+        public static int FormingMagicSquare(IEnumerable<int> s)
         {
-            var minChanges = int.MaxValue;
-
-            for (var i = 0; i < matrix.Length; ++i) //o
-            {
-                int changeCount = 0;
-                for (var x = 0; x < matrix.Length; ++x)
-                {
-                    for (var y = 0; y < matrix.Length; ++y)
-                    {
-                        changeCount += Math.Abs(matrixCombination[i, y, x] - matrix[y][x]);
-                    }
-                }
-
-                minChanges = Math.Min(minChanges, changeCount);
-            }
-
-            return minChanges;
+            return Squares
+                .Select(square => square.Zip(s, (m, t) => Math.Abs(m - t)).Sum())
+                .Min();
         }
     }
 }
