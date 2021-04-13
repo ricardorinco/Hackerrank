e8a7f167e13f129ae0637c2ce829d05b28ad9edb
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-13 06:40:25 -0300

    Added algorithm Angry Professor

---
+++ src/Hackerrank.ConsoleApp/033-AngryProfessor/AngryProfessorResolve.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class AngryProfessorResolve
+    {
+        public static string AngryProfessor(int threshold, IEnumerable<int> students)
+        {
+            var studentsArrived = students.Count(x => x <= 0);
+
+            return studentsArrived >= threshold ? "NO" : "YES";
+        }
+    }
+}
