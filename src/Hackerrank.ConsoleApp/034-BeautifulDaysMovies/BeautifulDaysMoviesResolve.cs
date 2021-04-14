55c47edce006986e233cd3f101de09e488b9d5d2
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-14 05:44:34 -0300

    Added algorithm Beautiful Days Movies

---
+++ src/Hackerrank.ConsoleApp/034-BeautifulDaysMovies/BeautifulDaysMoviesResolve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class BeautifulDaysMoviesResolve
+    {
+        public static int BeautifulDays(int startDay, int endDay, int divisor)
+        {
+            var days = 0;
+            for (var i = startDay; i <= endDay; i++)
+            {
+                var reverseString = new string(i.ToString().Reverse().ToArray());
+                var reverseNumber = int.Parse(reverseString);
+                var d = Math.Abs(i - reverseNumber);
+                var r = d % divisor;
+
+                if (r == 0)
+                    days++;
+            }
+
+            return days;
+        }
+    }
+}
