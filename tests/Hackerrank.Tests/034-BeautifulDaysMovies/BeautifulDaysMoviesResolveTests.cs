a3c164b8f620f713d404e92c582b1d43317548c0
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-14 05:44:54 -0300

    Added algorithm Beautiful Days Movies

---
+++ tests/Hackerrank.Tests/034-BeautifulDaysMovies/BeautifulDaysMoviesResolveTests.cs
@@ -0,0 +1,23 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class BeautifulDaysMoviesResolveTests
+    {
+        [Theory]
+        [InlineData(2, 20, 23, 6)]
+        [InlineData(33, 13, 45, 3)]
+        public void Should_be_the_class_is_cancelled_expect_results_match(
+            int expectedResult,
+            int startDay,
+            int endDay,
+            int divisor
+        )
+        {
+            var result = BeautifulDaysMoviesResolve.BeautifulDays(startDay, endDay, divisor);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
