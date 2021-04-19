3fc892ffbf934dba3f1c538eba500a894a6c611f
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-19 19:17:11 -0300

    Added algorithm Viral Advertising

---
+++ src/Hackerrank.ConsoleApp/035-ViralAdvertising/ViralAdvertisingResolve.cs
@@ -0,0 +1,19 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public static class ViralAdvertisingResolve
+    {
+        public static int ViralAdvertising(int numberOfDays)
+        {
+            int peopleLiked = 5;
+            int sum = 0;
+
+            for (var i = 0; i < numberOfDays; i++)
+            {
+                sum += peopleLiked / 2;
+                peopleLiked = peopleLiked / 2 * 3;
+            }
+
+            return sum;
+        }
+    }
+}
