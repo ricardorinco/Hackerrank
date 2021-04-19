4a6ef1166463a888306319b00b5219e3dc1106ae
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-19 19:17:05 -0300

    Added algorithm Viral Advertising

---
+++ tests/Hackerrank.Tests/035-ViralAdvertising/ViralAdvertisingResolveTests.cs
@@ -0,0 +1,18 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class ViralAdvertisingResolveTests
+    {
+        [Theory]
+        [InlineData(9, 3)]
+        [InlineData(15, 4)]
+        public void Should_be_how_many_people_have_liked_end_of_given_day_expect_results_match(int expectedResult, int numberOfDays)
+        {
+            var willGoOn = ViralAdvertisingResolve.ViralAdvertising(numberOfDays);
+
+            Assert.Equal(expectedResult, willGoOn);
+        }
+    }
+}
