95f75508188186c39c06c2342a8e2c4eee4934ac
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-20 08:34:35 -0300

    Added algorithm Save The Prisoner

---
+++ src/Hackerrank.ConsoleApp/036-SavePrisoner/SavePrisonerResolveTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Hackerrank.ConsoleApp
+{
+    public static class SavePrisonerResolve
+    {
+        public static int SaveThePrisoner(int numberOfPrisoners, int numberOfSweets, int prisonerId)
+        {
+            var remainingPrisonersBeforeEnd = numberOfPrisoners - prisonerId + 1;
+            if (numberOfSweets > remainingPrisonersBeforeEnd)
+            {
+                prisonerId = 1;
+                numberOfSweets -= remainingPrisonersBeforeEnd;
+                numberOfSweets %= numberOfPrisoners;
+            }
+
+            if (numberOfSweets == 0)
+                prisonerId = numberOfPrisoners;
+            else
+                prisonerId += numberOfSweets - 1;
+
+            return prisonerId;
+        }
+    }
+}
