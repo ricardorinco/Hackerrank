72c5fc41d5d0bd027ff7e0bf41d98761c34f6b95
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-20 08:34:22 -0300

    Added algorithm Save The Prisoner

---
+++ tests/Hackerrank.Tests/036-SavePrisoner/SavePrisonerResolveTests.cs
@@ -0,0 +1,25 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class SavePrisonerResolveTests
+    {
+        [Theory]
+        [InlineData(2, 5, 2, 1)]
+        [InlineData(3, 5, 2, 2)]
+        [InlineData(6, 7, 19, 2)]
+        [InlineData(3, 3, 7, 3)]
+        public void Should_be_chair_number_occupied__receive_candy_expect_results_match(
+            int expectedResult,
+            int numberOfPrisoners,
+            int numberOfSweets,
+            int prisonerId
+        )
+        {
+            var result = SavePrisonerResolve.SaveThePrisoner(numberOfPrisoners, numberOfSweets, prisonerId);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
