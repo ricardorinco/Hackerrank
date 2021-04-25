2b517f5d5fc9ea4e8e786b8d9637926d8c33d1b1
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-25 09:53:25 -0300

    Added algorithm Find Digits

---
+++ src/Hackerrank.ConsoleApp/039-FindDigits/FindDigitsResolve.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class FindDigitsResolve
+    {
+        public static int FindDigits(int numbers)
+        {
+            var divisors = 0;
+
+            var digits = numbers.ToString()
+                .Select(d => int.Parse(d.ToString()))
+                .ToList();
+
+            foreach (var digit in digits.Where(d => d > 0))
+            {
+                if (numbers % digit == 0)
+                    divisors++;
+            }
+
+            return divisors;
+        }
+    }
+}
