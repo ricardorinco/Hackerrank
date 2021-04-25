75d20863c853c30763fdb597fbc2198379787af0
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-24 21:21:43 -0300

    Added algorithm Sequence Equation

---
+++ tests/Hackerrank.Tests/038-SequenceEquation/SequenceEquationResolveTests.cs
@@ -0,0 +1,21 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class SequenceEquationResolveTests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 3, 1 }, new[] { 2, 3, 1 })]
+        [InlineData(new[] { 1, 3, 5, 4, 2 }, new[] { 4, 3, 5, 1, 2 })]
+        public void Should_be_find_the_number_of_sequences_expect_results_match(
+            int[] expectedResult,
+            int[] values
+        )
+        {
+            var results = SequenceEquationResolve.PermutationEquation(values);
+
+            Assert.Equal(expectedResult, results);
+        }
+    }
+}
