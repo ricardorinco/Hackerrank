335eab33f8d66d28cd7a2eceb8ea9feaf5fb0207
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-24 21:21:49 -0300

    Added algorithm Sequence Equation

---
+++ src/Hackerrank.ConsoleApp/038-SequenceEquation/SequenceEquationResolve.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hackerrank.ConsoleApp
+{
+    public static class SequenceEquationResolve
+    {
+        public static int[] PermutationEquation(int[] values)
+        {
+            int[] result = new int[values.Length];
+
+            for (int i = 1; i <= values.Length; i++)
+                result[i - 1] = Array.IndexOf(values, Array.IndexOf(values, i) + 1) + 1;
+
+            return result;
+        }
+    }
+}
