dfe703b3be995c677a7992a3b3b4d5e3401d1b2a
Ricardo Rinco <ricardo.rinco@hotmail.com>
2021-04-25 09:53:04 -0300

    Added algorithm Find Digits

---
+++ tests/Hackerrank.Tests/039-FindDigits/FindDigitsResolveTests.cs
@@ -0,0 +1,24 @@
+﻿using Hackerrank.ConsoleApp;
+using Xunit;
+
+namespace Hackerrank.Tests
+{
+    public class FindDigitsResolveTests
+    {
+        [Theory]
+        [InlineData(3, 124)]
+        [InlineData(3, 111)]
+        [InlineData(1, 10)]
+        [InlineData(2, 12)]
+        [InlineData(3, 1012)]
+        public void Should_be_the_number_of_divisors_expect_results_match(
+            int expectedResult,
+            int number
+        )
+        {
+            var results = FindDigitsResolve.FindDigits(number);
+
+            Assert.Equal(expectedResult, results);
+        }
+    }
+}
